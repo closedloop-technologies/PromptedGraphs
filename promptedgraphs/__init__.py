45d5f420977cd1078869c81afc2d1c05658228c6
Sean Kruzel <closedLoop@users.noreply.github.com>
2026-06-19 11:30:54 -0700

    Export prompted runtime primitives

--- promptedgraphs/__init__.py
+++ promptedgraphs/__init__.py
@@ -3,3 +3,16 @@ __version__ = "0.4.3"
 __description__ = (
     "From Dataset Labeling to Deployment: The Power of NLP and LLMs Combined."
 )
+
+from promptedgraphs.runtime import (  # noqa: F401
+    GraphOperation,
+    PromptedGraph,
+    PromptedLabel,
+    PromptedNode,
+    PromptedNodeSpec,
+    PromptedRun,
+    ResourceDelta,
+    ResourceRef,
+    SQLitePromptedStore,
+    prompted_node,
+)
